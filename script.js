25f09343eb93ac5e528275306bbfe3aa24ff7634
afilchock <afilchock@gmail.com>
2022-11-27 12:50:04 -0500

    setting up home page

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $("#buttonPrompt").click(function() {
-    let ask = prompt("Are you interested in food inequality? Y/N");
+    let ask = prompt("Are you interested in food inequality? yes/no");
     if (ask == 'no'){
-        $("h1").html("I'm just here to find my closest farmers market.");
+        $("h1").html("Just Show Me the Farmers Markets");
     } 
     else if (ask == 'yes'){
         alert("Great! Let's take a look.");
-        $("h1").html("Farmers Markets in New York City");
+        $("h1").html("NYC Farmers Markets in Relation to Food Deserts");
     };
 });
 $("#noShow").hide();
-// modify css styles for the button
-$("#toggleSongs").css({
+// modify css styles for the photo gallery button
+$("#togglePhotos").css({
     "background-color": "green",
     "color": "white",
     "font-size": "20px",
@@ -20,8 +20,29 @@ $("#toggleSongs").css({
     "margin": "10px",
     "cursor": "pointer"
 });
-$("#toggleSongs").click(function() {
-    $("#song").toggle();
+$("#togglePhotos").click(function() {
+    $("#photos").toggle();
+  });
+$("img").css({  
+    "border-radius": "25px",
+    "border": "2px solid #73AD21",
+    "padding": "20px",
+    "width": "200px",
+    "height": "150px",})
+$("#noShow").hide();
+// modify css styles for the bookshelf button
+$("#toggleBookshelf").css({
+    "background-color": "blue",
+    "color": "white",
+    "font-size": "20px",
+    "padding": "10px",
+    "border-radius": "5px",
+    "border": "2px solid black",
+    "margin": "10px",
+    "cursor": "pointer"
+});
+$("#toggleBookshelf").click(function() {
+    $("#bookshelf").toggle();
   });
 $("img").css({  
     "border-radius": "25px",
